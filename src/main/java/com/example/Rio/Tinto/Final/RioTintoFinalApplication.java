5d227496ac1f3edbaaed94343a8d122f685b84d1
nikola.m.vukovic <nikola.m.vukovic@hyperoptic.com>
2024-07-24 12:31:51 +0200

    # Initial commit

---
+++ src/main/java/com/example/Rio/Tinto/Final/RioTintoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.Rio.Tinto.Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RioTintoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RioTintoFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Rio/Tinto/Final/RioTintoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Rio.Tinto.Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RioTintoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
